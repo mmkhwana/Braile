6bc4c06dabce60108f06fc49a1521098a7d5f37d
mmkhwana <mmkhwana@student.wethinkcode.co.za>
2020-02-04 00:39:46 +0200

    so far

---
+++ braile.c
@@ -0,0 +1,418 @@
+#include <stdio.h>
+
+void	ft_braile(char *str)
+{
+	char a[3][2];
+	 a[0][0] = '#';
+	 a[0][1] = '*';
+	 a[1][0] = '*';
+	 a[1][1] = '*';
+	 a[2][0] = '*';
+	 a[2][1] = '*';
+
+	 char b[3][2];
+	 b[0][0] = '#';
+	 b[0][1] = '*';
+	 b[1][0] = '#';
+	 b[1][1] = '*';
+	 b[2][0] = '*';
+	 b[2][1] = '*';
+
+
+	 char c[3][2];
+	 c[0][0] = '#';
+	 c[0][1] = '#';
+	 c[1][0] = '*';
+	 c[1][1] = '*';
+	 c[2][0] = '*';
+	 c[2][1] = '*';
+
+
+	 char d[3][2];
+	 d[0][0] = '#';
+	 d[0][1] = '#';
+	 d[1][0] = '*';
+	 d[1][1] = '#';
+	 d[2][0] = '*';
+	 d[2][1] = '*';
+
+
+	 char e[3][2];
+	 e[0][0] = '#';
+	 e[0][1] = '*';
+	 e[1][0] = '*';
+	 e[1][1] = '#';
+	 e[2][0] = '*';
+	 e[2][1] = '*';
+
+
+	 char f[3][2];
+	 f[0][0] = '#';
+	 f[0][1] = '#';
+	 f[1][0] = '#';
+	 f[1][1] = '*';
+	 f[2][0] = '*';
+	 f[2][1] = '*';
+
+	 
+	 char g[3][2];
+	 g[0][0] = '#';
+	 g[0][1] = '#';
+	 g[1][0] = '#';
+	 g[1][1] = '#';
+	 g[2][0] = '*';
+	 g[2][1] = '*';
+
+
+	 char h[3][2];
+	 h[0][0] = '#';
+	 h[0][1] = '*';
+	 h[1][0] = '#';
+	 h[1][1] = '#';
+	 h[2][0] = '*';
+	 h[2][1] = '*';
+
+	 char i[3][2];
+	 i[0][0] = '*';
+	 i[0][1] = '#';
+	 i[1][0] = '#';
+	 i[1][1] = '*';
+	 i[2][0] = '*';
+	 i[2][1] = '*';
+
+
+	 char j[3][2];
+	 j[0][0] = '*';
+	 j[0][1] = '#';
+	 j[1][0] = '#';
+	 j[1][1] = '#';
+	 j[2][0] = '*';
+	 j[2][1] = '*';
+
+
+	 char k[3][2];
+	 k[0][0] = '#';
+	 k[0][1] = '*';
+	 k[1][0] = '*';
+	 k[1][1] = '*';
+	 k[2][0] = '#';
+	 k[2][1] = '*';
+
+	 char l[3][2];
+	 l[0][0] = '#';
+	 l[0][1] = '*';
+	 k[1][0] = '#';
+	 l[1][1] = '*';
+	 l[2][0] = '#';
+	 l[2][1] = '*';
+
+	 char m[3][2];
+	 m[0][0] = '#';
+	 m[0][1] = '#';
+	 m[1][0] = '*';
+	 m[1][1] = '*';
+	 m[2][0] = '#';
+	 m[2][1] = '*';
+
+	 char n[3][2];
+	 n[0][0] = '#';
+	 n[0][1] = '#';
+	 n[1][0] = '*';
+	 n[1][1] = '#';
+	 n[2][0] = '#';
+	 n[2][1] = '*';
+
+	 char o[3][2];
+	 o[0][0] = '#';
+	 o[0][1] = '*';
+	 o[1][0] = '*';
+	 o[1][1] = '#';
+	 o[2][0] = '#';
+	 o[2][1] = '*';
+
+	 char p[3][2];
+	 p[0][0] = '#';
+	 p[0][1] = '#';
+	 p[1][0] = '#';
+	 p[1][1] = '*';
+	 p[2][0] = '#';
+	 p[2][1] = '*';
+
+	 char q[3][2];
+	 q[0][0] = '#';
+	 q[0][1] = '#';
+	 q[1][0] = '#';
+	 q[1][1] = '#';
+	 q[2][0] = '#';
+	 q[2][1] = '*';
+
+	 char r[3][2];
+	 r[0][0] = '#';
+	 r[0][1] = '*';
+	 r[1][0] = '*';
+	 r[1][1] = '*';
+	 r[2][0] = '#';
+	 r[2][1] = '*';
+
+	 char s[3][2];
+	 s[0][0] = '*';
+	 s[0][1] = '#';
+	 s[1][0] = '#';
+	 s[1][1] = '*';
+	 s[2][0] = '#';
+	 s[2][1] = '*';
+
+	 char t[3][2];
+	 t[0][0] = '*';
+	 t[0][1] = '#';
+	 t[1][0] = '#';
+	 t[1][1] = '#';
+	 t[2][0] = '#';
+	 t[2][1] = '*';
+
+	 char u[3][2];
+	 u[0][0] = '#';
+	 u[0][1] = '*';
+	 u[1][0] = '*';
+	 u[1][1] = '*';
+	 u[2][0] = '#';
+	 u[2][1] = '#';
+
+	 char v[3][2];
+	 v[0][0] = '#';
+	 v[0][1] = '#';
+	 v[1][0] = '*';
+	 v[1][1] = '*';
+	 v[2][0] = '#';
+	 v[2][1] = '#';
+
+	 char w[3][2];
+	 w[0][0] = '*';
+	 w[0][1] = '#';
+	 w[1][0] = '#';
+	 w[1][1] = '#';
+	 w[2][0] = '*';
+	 w[2][1] = '#';
+
+	 char x[3][2];
+	 x[0][0] = '#';
+	 x[0][1] = '#';
+	 x[1][0] = '*';
+	 x[1][1] = '*';
+	 x[2][0] = '#';
+	 x[2][1] = '#';
+
+	 char y[3][2];
+	 y[0][0] = '#';
+	 y[0][1] = '#';
+	 y[1][0] = '*';
+	 y[1][1] = '#';
+	 y[2][0] = '#';
+	 y[2][1] = '#';
+
+	 char z[3][2];
+	 z[0][0] = '#';
+	 z[0][1] = '*';
+	 z[1][0] = '*';
+	 z[1][1] = '#';
+	 z[2][0] = '#';
+	 z[2][1] = '#';
+	 
+	 int	iter1 = 0;
+	 while ( iter1 < 3)
+	 {
+		 int	iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",a[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",b[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",c[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",d[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",e[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",f[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",g[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",h[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",i[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",j[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",k[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",l[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",m[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",n[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",o[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",p[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",q[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",r[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",s[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",t[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",u[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",v[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",w[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",x[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",y[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf(" ");
+		 iter2 = 0;
+		 while (iter2 < 2)
+		 {
+			 printf("%c",z[iter1][iter2]);
+			 iter2++;
+		 }
+		 printf("\n");
+		 iter1++;
+	 }
+}
+
+int	main(int ac, char **av)
+{
+	int i = 0;
+	if (ac > 1)
+	{
+		ft_braile(av[i]);
+	}
+	return (0);
+}
